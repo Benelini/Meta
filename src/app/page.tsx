d2815d51f01d1fc821c2f212fe52d25e9f06b0d2
Benelini <eli.bencur@gmail.com>
2023-12-20 10:13:09 +0100

    feat: main page with search

---
+++ src/app/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
+type Movie = {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+};
+
+export default function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movieData, setMovieData] = useState<Movie[]>([]);
+
+  // Fetch movies from OMDb API
+  const fetchMovies = async (searchValue: string) => {
+    const apiKey = process.env.NEXT_PUBLIC_OMDB_API_KEY;
+    const response = await fetch(
+      `http://www.omdbapi.com/?s=${encodeURIComponent(
+        searchValue
+      )}&apikey=${apiKey}`
+    );
+    const data = await response.json();
+    if (data.Search) {
+      setMovieData(data.Search);
+    } else {
+      setMovieData([]);
+    }
+  };
+
+  // Debouncing
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      if (searchTerm) {
+        fetchMovies(searchTerm);
+      } else {
+        setMovieData([]);
+      }
+    }, 500);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchTerm]);
+
+  return (
+    <main className="flex flex-col justify-start items-center p-4 bg-mainIndigo min-h-[100vh] h-fit">
+      <input
+        className="text-black rounded-md w-1/3 py-2 text-2xl mb-4 px-2"
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Search Movies"
+      />
+      {movieData && movieData.length > 0 && (
+        <ul className="bg-darkGreen rounded-2xl">
+          {movieData.map((movie) => (
+            <li className="flex flex-row mb-2 py-4 px-4" key={movie.imdbID}>
+              <Image
+                className="mr-4 rounded-md"
+                src={movie.Poster}
+                alt={movie.Title}
+                width={75}
+                height={111}
+              ></Image>
+              <div className="flex flex-col ">
+                <Link href={`/movies/${movie.imdbID}`}>
+                  <h2>
+                    {movie.Title} ({movie.Year})
+                  </h2>
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </main>
+  );
+}
