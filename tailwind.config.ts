da7f77eccb4256c22aefcb959a3ce726d6520b02
Benelini <eli.bencur@gmail.com>
2023-12-20 10:11:05 +0100

    chore: remove redundant classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,11 +16,6 @@ const config: Config = {
         lightGreen: "#9EC8B9"
 
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
