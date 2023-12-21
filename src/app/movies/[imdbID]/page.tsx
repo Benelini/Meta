b358893518e026a2f4c51e410e36a33c95b14b25
Benelini <eli.bencur@gmail.com>
2023-12-21 13:12:55 +0100

    feat: back button on movie page, small design changes

--- src/app/movies/[imdbID]/page.tsx
+++ src/app/movies/[imdbID]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import BackButton from "@/app/components/svgs/back-button";
 import Image from "next/image";
+import Link from "next/link";
 import { useState, useEffect } from "react";
 
 type TMovieDetail = {
@@ -68,7 +70,11 @@ const MovieDetail = ({ params }: TMovieDetail) => {
   }, [imdbID]);
 
   if (loading) {
-    return <div className="self-center">Loading...</div>;
+    return (
+      <div className="self-center h-screen bg-mainIndigo w-full justify-center items-center flex flex-col">
+        Loading...
+      </div>
+    );
   }
 
   if (error) {
@@ -78,46 +84,57 @@ const MovieDetail = ({ params }: TMovieDetail) => {
   return (
     <div className="flex flex-row bg-mainIndigo justify-center items-start w-screen text-center h-[100vh]">
       {movie && (
-        <div className="flex flex-row justify-start items-center bg-darkGreen rounded-md px-4 py-4 max-w-2xl mt-10">
-          <Image
-            className="mr-4 rounded-md"
-            src={movie.Poster}
-            alt={movie.Title}
-            width={260}
-            height={400}
-          ></Image>
-          <div className="flex flex-col font-bold text-left gap-1">
-            <h1 className="text-4xl font-bold text-left">{movie.Title}</h1>
-            <p>{movie.Genre}</p>
-            <div className="flex flex-row place-items-center gap-1">
-              <p>{movie.Country}, </p>
-              <p>{movie.Year}</p>
+        <div className="flex flex-col justify-start items-center bg-darkGreen rounded-md px-4 py-4 max-w-2xl mt-10 pb-12">
+          <div className="flex flex-row justify-between items-center text-center w-full pb-2">
+            <Link className="py-2 text-4xl font-bold" href="/">
+              <BackButton className="fill-white" />
+            </Link>
+            <h1 className="text-4xl font-bold text-center w-full">
+              {movie.Title}
+            </h1>
+            <div className="w-6"></div>
+          </div>
+
+          <div className="flex flex-row items-start justify-start">
+            <Image
+              className="mr-4 rounded-md"
+              src={movie.Poster}
+              alt={movie.Title}
+              width={260}
+              height={400}
+            ></Image>
+            <div className="flex flex-col font-bold text-left gap-1">
+              <p>{movie.Genre}</p>
+              <div className="flex flex-row place-items-center gap-1">
+                <p>{movie.Country}, </p>
+                <p>{movie.Year}</p>
+              </div>
+
+              <p>
+                Director: <span className="font-normal">{movie.Director}</span>{" "}
+              </p>
+
+              <p className="flex flex-row place-items-center">
+                Rating:
+                <span className="font-normal ml-1">
+                  {" "}
+                  {movie.imdbRating} / 10{" "}
+                </span>
+                <svg
+                  className="ml-1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                >
+                  <path
+                    fill="yellow"
+                    d="m12 17.27 4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72 3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"
+                  />
+                </svg>
+              </p>
+              <p className="text-sm font-normal">{movie.Plot}</p>
             </div>
-
-            <p>
-              Director: <span className="font-normal">{movie.Director}</span>{" "}
-            </p>
-
-            <p className="flex flex-row place-items-center">
-              Rating:
-              <span className="font-normal ml-1">
-                {" "}
-                {movie.imdbRating} / 10{" "}
-              </span>
-              <svg
-                className="ml-1"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-              >
-                <path
-                  fill="yellow"
-                  d="m12 17.27 4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72 3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"
-                />
-              </svg>
-            </p>
-            <p className="text-sm font-normal">{movie.Plot}</p>
           </div>
         </div>
       )}
